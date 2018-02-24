81cfe0b015a440e51bfd47c1e7763e4d8a8aa172
Victor Rose <vicdrose@users.noreply.github.com>
2018-02-24 13:55:34 -0800

    Admob maybe not woes
    Let's see

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,29 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "cordova-connectivity-monitor.connectivity",
+    "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+    "pluginId": "cordova-connectivity-monitor",
+    "clobbers": [
+      "window.connectivity"
+    ]
+  },
+  {
+    "id": "cordova-admob.AdMobAds",
+    "file": "plugins/cordova-admob/www/admob.js",
+    "pluginId": "cordova-admob",
+    "clobbers": [
+      "window.admob",
+      "window.tappx"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-connectivity-monitor": "1.2.2",
+  "cordova-admob": "4.1.16"
 };
 // BOTTOM OF METADATA
 });
