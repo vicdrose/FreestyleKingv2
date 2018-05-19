42d95c81402cf785670ff569c78eb703b058005e
Victor Rose <vicdrose@users.noreply.github.com>
2018-05-18 23:10:45 -0500

    Updated Randomizer
    Slower transitions

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -16,6 +16,27 @@ module.exports = [
       "window.admob",
       "window.tappx"
     ]
+  },
+  {
+    "id": "cordova-promise-polyfill.Promise",
+    "file": "plugins/cordova-promise-polyfill/www/Promise.js",
+    "pluginId": "cordova-promise-polyfill",
+    "runs": true
+  },
+  {
+    "id": "cordova-promise-polyfill.promise.min",
+    "file": "plugins/cordova-promise-polyfill/www/promise.min.js",
+    "pluginId": "cordova-promise-polyfill"
+  },
+  {
+    "id": "cordova-plugin-admob-free.AdMob",
+    "file": "plugins/cordova-plugin-admob-free/www/admob.js",
+    "pluginId": "cordova-plugin-admob-free",
+    "clobbers": [
+      "admob",
+      "AdMob",
+      "plugins.AdMob"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -23,7 +44,11 @@ module.exports.metadata =
 {
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-connectivity-monitor": "1.2.2",
-  "cordova-admob": "4.1.16"
+  "cordova-admob": "4.1.16",
+  "cordova-promise-polyfill": "0.0.2",
+  "cordova-admob-sdk": "0.16.0",
+  "cordova-plugin-admob-free": "0.16.2",
+  "cordova-google-play-services": "25.0.0"
 };
 // BOTTOM OF METADATA
 });
